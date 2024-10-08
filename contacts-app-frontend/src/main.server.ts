c8fde95691d8d8fe2af2bcceaac5c87d315aebee
Dmitry Ivanov <dimas731fox@gmail.com>
2024-10-08 19:23:12 +0300

    third commit

--- contacts-app-frontend/src/main.server.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { config } from './app/app.config.server';
-import { ContactListComponent } from './app/contact-list/contact-list.component';
-import { AppComponent } from './app/app.component';
-
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
